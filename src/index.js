f62351f220ebb7a6620d755a1c34c46cb53e50f0
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-08-20 00:09:01 -0300

    Fixes based on v4 Switch statment

--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ import PageTwo from './PageTwo';
 import PageThree from './PageThree';
 import NotFound from './NotFound';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
     <div>
-      <Route path="/" component={App}></Route>
-      <Route path="/PageOne" component={PageOne}></Route>
-      <Route path="/PageTwo" component={PageTwo}></Route>
-      <Route path="/PageThree" component={PageThree}></Route>
-      <Route path="*" component={NotFound}></Route>
+      <Switch  >
+        <Route exact path="/" component={App} />
+        <Route path="/PageOne" component={PageOne}></Route>
+        <Route path="/PageTwo" component={PageTwo}></Route>
+        <Route path="/PageThree" component={PageThree}></Route>
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </Router>,
   document.getElementById('root'));
